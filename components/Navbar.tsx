32e5db62413611b223d4b3d2efe33ec4f0c19c5c
Ajarek <ajarek@poczta.onet.pl>
2024-08-03 00:35:39 +0200

    added template

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+
+const Navbar = () => {
+  return (
+    <div className="w-3/4 mx-auto h-16 flex items-center justify-between px-4 border-b border-primary bg-background ">
+      <Link href="/" className="flex items-center gap-2">
+      <Image
+              src="/images/logo.png"
+              alt="Notes Logo"
+              className="flex items-center gap-2"
+              width={48}
+              height={48}
+              priority
+            />
+            <h1 className="text-2xl"><span className="font-bold">Quick</span> Notes</h1>
+
+      </Link>
+      <Button variant="outline" className="text-primary text-lg ">Add a note</Button>
+        
+    </div>
+  )
+}
+
+export default Navbar
